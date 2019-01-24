b74b96ff5d3b0dd6709db612980676ce06b3f92a
Maxence Charriere <charriere@outlook.com>
2019-01-24 12:51:03 -0800

    change example target

--- example/remote/main.go
+++ example/remote/main.go
@@ -3,5 +3,11 @@ package main
 import "github.com/maxence-charriere/murlok"
 
 func main() {
+	murlok.AllowHosts(
+		"www.theverge.com",
+		"store.theverge.com",
+		"https://the-verge.myshopify.com/",
+	)
+
 	murlok.Run("https://www.theverge.com")
 }
