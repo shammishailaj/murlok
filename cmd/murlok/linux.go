7ab27d30f4451abe48fc2d6a2d85a5a4d24775c7
Maxence Charriere <charriere@outlook.com>
2019-01-27 22:31:57 -0800

    fix linux murlok build

---
+++ cmd/murlok/linux.go
@@ -0,0 +1,17 @@
+// +build linux
+
+package main
+
+import "context"
+
+func build(ctx context.Context, args []string) {
+	fail("not implemented")
+}
+
+func run(ctx context.Context, args []string) {
+	fail("not implemented")
+}
+
+func clean(ctx context.Context, args []string) {
+	fail("not implemented")
+}
