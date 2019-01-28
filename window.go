00d37bcbdb5ebed004f1856e4f0a3d7d6cd57133
Maxence Charriere <charriere@outlook.com>
2019-01-27 19:49:47 -0800

    set mac window text color

--- window.go
+++ window.go
@@ -2,16 +2,19 @@ package murlok
 
 // Window is a struct that describes a window.
 type Window struct {
-	// The url load when the view is created.
-	URL string
-
 	// The background color (#rrggbb).
 	BackgroundColor string `json:",omitempty"`
 
 	// Sets the background as a frosted surface.
 	FrostedBackground bool `json:",omitempty"`
 
-	// Sets a title bar color. When set, the web content appears below the title
-	// bar rather than under.
+	// The text color (#rrggbb).
+	TextColor string `json:",omitempty"`
+
+	// The title bar color (#rrggbb). When set, the web content appears below
+	// the title bar rather than under.
 	TitleBarColor string `json:",omitempty"`
+
+	// The url load when the view is created.
+	URL string
 }
